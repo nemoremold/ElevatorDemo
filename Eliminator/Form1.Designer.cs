4b919d5a7dfe7dd72915e17b764876b5c071be58
Emoin.Lam <nemoremold@gmail.com>
2017-05-01 15:52:33 +0800

    added: data structure and core codes

--- Eliminator/Form1.Designer.cs
+++ Eliminator/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1222, 621);
+            this.ClientSize = new System.Drawing.Size(774, 611);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
